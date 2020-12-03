d479a660877593de5ff22bd480e47801c45e3548
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-12-03 14:34:16 -0700

    pushing to class

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ class _Node{
         this.next = next;
     }
 }
+// create stack indicating that top is empty 
+class starTrek {
+    constructor(){
+        this.top = null 
+    }
+    push(data){
+        //if stack is empty then this node will be new top of stack
+        if(this.top === null){
+            this.top = new _Node(data,null)
+            return this.top
+        }
+    }
+}
+push("Kirk","Scotty","Spock")
